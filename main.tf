581452d1c3a118f698737177984b47416763edca
rishi0803 <128279431+rishi0803@users.noreply.github.com>
2023-07-04 10:56:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2_instance" {
-        access_key = "AKIAUF2O4HCY3TWWKLTR"
-        secret_key = "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"
         count = 1
         ami = "ami-024e6efaf93d85776"
         instance_type = "t2.micro"
